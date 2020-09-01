f59d9f8d0899dae946720e74a0c842ac12d735d2
Victor Osório <victor.perticarrari@gmail.com>
2020-09-01 11:46:51 -0300

    Fixing visualization

--- src/lib/matrix-config.c
+++ src/lib/matrix-config.c
@@ -9,8 +9,8 @@ matrix_config_t *matrix_config_initialize(size_t width, size_t height)
     matrix_config_t *config = (matrix_config_t *)malloc(sizeof(matrix_config_t));
     config->columns = width;
     config->heights = height;
-    config->column_width = (size_t *)malloc(width * sizeof(size_t));
-    config->line_height = (size_t *)malloc(height * sizeof(size_t));
+    config->column_width = (size_t *)calloc(width, sizeof(size_t));
+    config->line_height = (size_t *)calloc(height, sizeof(size_t));
     return config;
 }
 
